ba761fafbe5d2c7cf02faa8c93cbc828e9f8e17b
qbmine001 <166589920+qbmine001@users.noreply.github.com>
2024-04-10 20:23:25 +0530

    Create apool.sh

---
+++ apool.sh
@@ -0,0 +1,3 @@
+wget https://github.com/apool-io/apoolminer/releases/download/v1.2.1/apoolminer_linux_v1.2.1.tar
+tar -xvf apoolminer_linux_v1.2.1.tar
+././apoolminer --account CP_7cqdjrvl2n --gpu-off --pool qubic1.hk.apool.io:3334 -t 0
